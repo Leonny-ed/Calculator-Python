1f91565f1a7e0518e58972b9ed9f57f2b2a0a297
Leonny-ed <94757096+Leonny-ed@users.noreply.github.com>
2022-01-30 15:22:32 +0000

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,74 @@
+    
+# simple calculator  @curious ProgrammeR           
+
+from tkinter import *
+# Oprations in calcuator
+def click(event):
+    global scvalue
+    text = event.widget.cget("text")
+    if text == '=':
+        if scvalue.get().isdigit():
+            value = int(scvalue.get())
+        else:
+            value = eval(screen.get())
+
+        scvalue.set(value)
+        screen.update()     
+    elif text == 'C':
+        scvalue.set("")
+        screen.update()
+    else:
+        scvalue.set(scvalue.get() + text )     
+        screen.update()
+
+window = Tk()
+window.geometry("600x650")
+window.minsize(600,650)
+window.maxsize(600,650)
+window.config(bg="gray")
+window.title("Calcuator By Curious ProgrammeR ")
+icon = PhotoImage(file = 'icon.png ')   
+window.iconphoto(False, icon)
+
+scvalue = StringVar()
+scvalue.set("")
+f = Frame(window, padx=20, pady=20)
+screen = Entry(f, textvar= scvalue, font = "lucida 50 bold", bg='light blue')
+screen.pack(fill=X, padx=20 , pady=15)
+f.pack()
+
+#options Rows 
+options1 = ["7", "8", "9", "+"] 
+options2 = ["4", "5", "6", "-"]
+options3 = ["1", "2", "3", "*"]
+options4 = ["0", "C", "=", "/"]
+
+f = Frame(window, bg="gray", padx=30, pady=10)
+for i in options1:
+        b = Button(f, text=i, padx=10, pady=10, font = "lucida 25 bold")
+        b.pack(side=LEFT, padx= 10,pady=10)
+        b.bind("<Button-1>", click)
+f.pack()
+
+f = Frame(window, bg="gray", padx=30, pady=10)
+for i in options2:
+        b = Button(f, text=i, padx=10, pady=10, font = "lucida 25 bold")
+        b.pack(side=LEFT, padx= 10,pady=10)
+        b.bind("<Button-1>", click)
+f.pack()
+
+f = Frame(window, bg="gray", padx=30, pady=10)
+for i in options3:
+        b = Button(f, text=i, padx=10, pady=10, font = "lucida 25 bold")
+        b.pack(side=LEFT, padx= 10,pady=10)
+        b.bind("<Button-1>", click)
+f.pack()
+
+f = Frame(window, bg="gray", padx=30, pady=10)
+for i in options4:
+        b = Button(f, text=i, padx=10, pady=10, font = "lucida 25 bold")
+        b.pack(side=LEFT, padx= 10,pady=10)
+        b.bind("<Button-1>", click)
+f.pack()
+
+window.mainloop()
